f60ebee5474e56d73ff0a024b037198b6e43b0ee
Emre Memiş <dev.emrememis@gmail.com>
2023-10-15 17:07:21 +0300

    add react icons and logo icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { BiSolidKeyboard } from "react-icons/bi";
 const App = () => {
   return (
     <div>
       <h1 className="text-sky-400">Ide Type</h1>
+      <BiSolidKeyboard className="text-red-300" />
     </div>
   );
 };
